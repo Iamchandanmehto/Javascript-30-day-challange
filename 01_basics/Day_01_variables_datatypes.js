f8e634acf96740a131c4cf3d2b65d842142d6550
Chandan Mehto <138262306+Iamchandanmehto@users.noreply.github.com>
2024-07-19 17:16:12 +0000

    Day_01:variables and datatypes

---
+++ 01_basics/Day_01_variables_datatypes.js
@@ -0,0 +1,58 @@
+/*Activity-1 variable declaration
+task 1 : declare a variable using var, assign it a number, and log the value to the console.
+task 1 : declare a variable using let, assign it a string, and log the value to the console.
+*/
+
+var mark = 1234;
+console.log(mark);
+
+let girl = "subhashi"
+console.log(girl);
+
+/*
+Activity-2 Constant declaration
+task 3 : declare a variable using const, assign it a boolean value, and log the value to the console.
+*/
+const isLoggedIn = true;
+console.log(isLoggedIn);
+
+/*
+Activity-3 Data Types
+task 4 : creat a variable of different data types(nubmer , string, boolean, array, object) and log each variable's using the type of operater
+*/
+
+let Contact = 991213999;
+console.log(typeof Contact); 
+// =>number
+
+const studentName = "subhashi"
+console.log(typeof studentName);
+//=>string
+
+ const isStudentPass = true
+console.log(typeof isStudentPass);
+//=>boolean
+
+let studentInClass = {
+    subhashi:"12th",
+    Abhinav:"11th"
+}
+console.log( typeof studentInClass);
+//=> Object
+
+let XIIStudents = ["subhashi", "Hinata", "Abhinav", "shradha"]
+console.log(typeof XIIStudents);
+//=> Objects
+
+//Activity-4
+//taks-5
+let myMarksInMaths= 400
+console.log(myMarksInMaths);
+
+myMarksInMaths = 499
+console.log(myMarksInMaths);
+
+const contact = 993349 
+console.log(contact);
+contact = 8483949
+console.log(contact);
